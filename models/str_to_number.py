66ed91782676d629f597e5aeecfce2ae49ed329b
David Rosero C <davidroserocalle@gmail.com>
2020-02-02 20:09:11 -0500

    Update function, fixing bug in any variable finish in ty

--- models/str_to_number.py
+++ models/str_to_number.py
@@ -25,17 +25,29 @@ def str_to_num(str_num):
     million = ""
     thousand = ""
     hundred = ""
+    ninety = ""
+    eighty = ""
+    seventy = ""
+    sixty = ""
     # Initializing variables to put the result in integers
     total_infected = 0
     billion_nums = 1
     million_nums = 1
     thousand_nums = 1
     hundred_nums = 1
+    ninety_nums = 0
+    eighty_nums = 0
+    seventy_nums = 0
+    sixty_nums = 0
     # Regex to get the patterns in words
     pattern_billion = '(bi)...(on)'
     pattern_million = '(mi)...(on)'
     pattern_thousand = '(tho)..(and)'
     pattern_hundred = '(hu)...(ed)'
+    pattern_ninety = '(ni)...(y)'
+    pattern_eighty = '(ei)...(y)'
+    pattern_seventy = '(se)....(y)'
+    pattern_sixty = '(si)..(y)'
 
     # Capture the data in strings for
     # B, M, T, H
@@ -67,6 +79,41 @@ def str_to_num(str_num):
             str_num = str_num[res.span()[1]:]
         else:
             pass
+    # Capture the data string for
+    # ninety, eighty, seventy, sixty and fourty
+    if re.search(pattern_ninety, str_num):
+        res = re.search(pattern_ninety, str_num)
+        if res.span()[0] == 0:
+            ninety = str_num[:res.span()[1]]
+            str_num = str_num[res.span()[1]:]
+        else:
+            pass
+
+    if re.search(pattern_eighty, str_num):
+        res = re.search(pattern_eighty, str_num)
+        if res.span()[0] == 0:
+            eighty = str_num[:res.span()[1]]
+            str_num = str_num[res.span()[1]:]
+        else:
+            pass
+    
+    if re.search(pattern_seventy, str_num):
+        res = re.search(pattern_seventy, str_num)
+        if res.span()[0] == 0:
+            seventy = str_num[:res.span()[1]]
+            str_num = str_num[res.span()[1]:]
+        else:
+            pass
+
+    if re.search(pattern_sixty, str_num):
+        res = re.search(pattern_sixty, str_num)
+        if res.span()[0] == 0:
+            sixty = str_num[:res.span()[1]]
+            str_num = str_num[res.span()[1]:]
+            print(sixty)
+        else:
+            pass
+    
     # Pass the string representation to number
     if not billion:
         billion_nums = 0
@@ -92,13 +139,36 @@ def str_to_num(str_num):
         hundred_infected = [value for key, value in numbers.items() if key in hundred]
         for infected in hundred_infected:
             hundred_nums *= infected
+    # Verify the numbers
+    if not ninety:
+        ninety_nums = 0
+    else:
+        ninety_nums = 90
+    if not eighty:
+        eighty_nums = 0
+    else:
+        eighty_nums = 80
+
+    if not seventy:
+        seventy_nums = 0
+    else:
+        seventy_nums = 70
+    if not sixty:
+        sixty_nums = 0
+    else:
+        sixty_nums = 60
+    # Sum the normal numbers
     num_infected = [value for key, value in numbers.items() if key in str_num]
     for infected in num_infected:
             total_infected += infected
     # Checking the data type
     for key in numbers.keys():
         if key in str_num:
-            total_infected = total_infected + billion_nums + million_nums + thousand_nums + hundred_nums
+            total_infected = (
+                total_infected + billion_nums + million_nums +
+                thousand_nums + hundred_nums + ninety_nums +
+                eighty_nums + seventy_nums + sixty_nums
+                )
         else:
             continue
     if total_infected == 0:
